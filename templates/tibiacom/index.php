e3f9f9ae57b6cbf1018ef99a3105864e618dfc3f
Mortera-world <116396805+Mortera-world@users.noreply.github.com>
2026-05-14 19:38:33 -0600

    Remove InfoBar section from tibiacom layout

--- templates/tibiacom/index.php
+++ templates/tibiacom/index.php
@@ -497,69 +497,6 @@ if (isset($config['boxes']))
                                  style="background-image:url(<?= $template_path; ?>/images/global/content/corner-tr.gif);"></div>
                             <div class="Border_1"
                                  style="background-image:url(<?= $template_path; ?>/images/global/content/border-1.gif);"></div>
-                            <div class="BorderTitleText"
-                                 style="background-image:url(<?= $template_path; ?>/images/global/content/newsheadline_background.gif); height: 28px;">
-                                <div class="InfoBar">
-                                    <img class="InfoBarBigLogo"
-                                         src="<?= $template_path; ?>/images/global/header/icon-download.png">
-                                    <span class="InfoBarNumbers">
-                                        <a class="InfoBarLinks" href="?subtopic=downloadclient"><span
-                                                    class="InfoBarSmallElement">Download Client</span></a>
-                                    </span>
-
-                                    <?php if (!empty($config['whatsapp_link'])) { ?>
-                                        <img class="InfoBarBigLogo" style="margin-left: 8px"
-                                             src="<?= $template_path; ?>/images/global/header/icon-whatsapp.png"
-                                             width="16">
-                                        <span class="InfoBarNumbers">
-                                            <a class="InfoBarLinks" href="<?= $config['whatsapp_link']; ?>" target="new"><span
-                                                        class="InfoBarSmallElement">Grupo Whatsapp</span></a>
-                                        </span>
-                                    <?php } ?>
-                                    <?php if (!empty($config['discord_link'])) { ?>
-                                        <img class="InfoBarBigLogo" style="margin-left: 8px"
-                                             src="<?= $template_path; ?>/images/global/header/icon-discord.png">
-                                        <span class="InfoBarNumbers">
-                                            <a class="InfoBarLinks" href="<?= $config['discord_link']; ?>" target="new"><span
-                                                        class="InfoBarSmallElement">Discord</span></a>
-                                        </span>
-                                    <?php } ?>
-                                    <?php if (!empty($config['instagram_link'])) { ?>
-                                        <img class="InfoBarBigLogo" style="margin-left: 8px"
-                                             src="<?= $template_path; ?>/images/global/header/icon-instagram.png"
-                                             width="16">
-                                        <span class="InfoBarNumbers">
-                                            <a class="InfoBarLinks" href="https://www.instagram.com/<?= $config['instagram_link']; ?>"
-                                               target="new"><span class="InfoBarSmallElement">Instagram</span></a>
-                                        </span>
-                                    <?php } ?>
-                                    <?php if (!empty($config['facebook_link'])) { ?>
-                                        <img class="InfoBarBigLogo" style="margin-left: 8px"
-                                             src="<?= $template_path; ?>/images/global/header/icon-facebook.png"
-                                             width="16">
-                                        <span class="InfoBarNumbers">
-                                            <a class="InfoBarLinks" href="https://www.facebook.com/<?= $config['facebook_link']; ?>"
-                                               target="new"><span class="InfoBarSmallElement">Facebook</span></a>
-                                        </span>
-                                    <?php } ?>
-                                    <span style="float: right; margin-top: 1px; margin-right: 4px">
-                                        <img class="InfoBarBigLogo" src="<?= $template_path; ?>/images/global/header/icon-players-online.png">
-                                        <span class="InfoBarNumbers">
-                                            <span class="InfoBarSmallElement">
-                                                <a class="InfoBarLinks" href="?online">
-                                                    <?= $status['online'] ? $status['playersTotal'] . ' Players Online' : 'Server Offline' ?>
-                                                </a>
-                                            </span>
-                                        </span>
-                                        <?php if ($config['collapse_status']) { ?>
-                                            <a data-bs-toggle="collapse" href="#statusbar" role="button" aria-expanded="false" aria-controls="statusbar">
-                                                <img src="<?= $template_path; ?>/images/global/content/top-to-back.gif" class="InfoBarBigLogo">
-                                            </a>
-                                        <?php } ?>
-                                    </span>
-
-                                </div>
-                            </div>
                             <!-- COLLAPSE STATUS BAR -->
                             <?php if ($config['collapse_status']) { ?>
                                 <div class="collapse" id="statusbar" style="background-color: #d4c0a1;">
